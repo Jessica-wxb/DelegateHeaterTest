0a49cc66058532b879c712f44197d87cf0bc1906
wangxiaobo <18831645427@163.com>
2020-02-03 10:58:32 +0800

    撤销更改

--- src/DeledateTest/DisplayListener.java
+++ src/DeledateTest/DisplayListener.java
@@ -10,6 +10,5 @@ package DeledateTest;
 public class DisplayListener {
     public void showTemperature(Integer date) {
         System.out.println("水烧开时显示的温度是："+date);
-        System.out.println("测试git提交是否成功");
     }
 }
